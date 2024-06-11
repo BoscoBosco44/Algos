15935a5c8734de498801de82ba178887dfb33bf9
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-06-11 16:40:26 -0500

    Update G75-ValidParentheses.js

--- G75-ValidParentheses.js
+++ G75-ValidParentheses.js
@@ -32,3 +32,38 @@ for(var i = 0; i < s.length - 1; i=i+2) {
         return false;
     }
 }
+
+
+
+
+
+
+
+//----------- first faild solution (fails on {[]}) ---------
+for(var i = 0; i < s.length - 1; i=i+2) {
+    var l1 = s.charAt(i);
+    var l2 = s.charAt(i+1);
+
+    if(l1 == '(' && l2 == ')') {
+        if(i == s.length - 2) {
+            return true;
+        } else {
+            continue;
+        }
+    } else if (l1 == '[' && l2 == ']') {
+        if(i == s.length - 2) {
+            return true;
+        } else {
+            continue;
+        }
+    } else if (l1 == '{' && l2 == '}') {
+        if(i == s.length - 2) {
+            return true;
+        } else {
+            continue;
+        }
+    }
+    else {
+        return false;
+    }
+}
