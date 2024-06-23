dde2567587154f086380dd99ed26bdbea10586f3
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-06-23 16:51:01 -0500

    Update G75-ValidParentheses.js

--- G75-ValidParentheses.js
+++ G75-ValidParentheses.js
@@ -67,3 +67,86 @@ for(var i = 0; i < s.length - 1; i=i+2) {
         return false;
     }
 }
+
+
+//-------------------------------
+
+public class Solution {
+    public
+        
+        bool matching(char a, char b) {
+            return ((a == '(' && b == ')') || (a == '{' && b == '}') || (a == '[' && b == ']'));
+        }
+    
+        
+        bool IsValid(string s) {
+            stack<int> stk; 
+            for(int i = 0; i < s.length(); i++) {
+                if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                    stk.push(s[i]);
+                }
+                else {
+                    if(stk.empty() == true) {
+                        return false;
+                    }
+                    else if(matching(stk.top(), s[i]) == false) {
+                        return false;
+                    }
+                    else {
+                        stk.pop();
+                    }
+                }
+            }
+            return (stk.empty() == true);
+        }
+    };
+
+
+    //----------------------
+
+    public class Solution {
+        public bool IsValid(string s) {
+            if (s.Length <= 1)
+                return false;
+    
+            var solList = new List<int>();
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    solList.Add(1);
+                    continue;       
+                }
+                if (s[i] == '[')
+                {
+                    solList.Add(2);
+                    continue;       
+                }
+                if (s[i] == '{')
+                {
+                    solList.Add(3);
+                    continue;       
+                }
+                if (solList.Count == 0)
+                    return false;
+                if (s[i] == ')' && solList[solList.Count - 1] == 1)
+                {
+                    solList.RemoveAt(solList.Count - 1);
+                    continue;
+                }
+                if (s[i] == ']' && solList[solList.Count - 1] == 2)
+                {
+                    solList.RemoveAt(solList.Count - 1);
+                    continue;
+                }
+                if (s[i] == '}' && solList[solList.Count - 1] == 3)
+                {
+                    solList.RemoveAt(solList.Count - 1);
+                    continue;
+                }
+                
+                return false;
+            }
+            return solList.Count == 0;
+        }
+    }
