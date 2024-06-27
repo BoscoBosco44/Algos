bb0048218e20be2a703877d4951f0274317f63a8
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-06-27 13:56:48 -0500

    Update G75-4-BuySellStock.cs

--- G75-4-BuySellStock.cs
+++ G75-4-BuySellStock.cs
@@ -1,23 +1,20 @@
-public int MaxProfit(int[] prices) {
-    int smlIndex = 0; // Buy day
-    int lrgIndex = 0; // Sell day
-    int profit = 0;
+    public int MaxProfit(int[] prices) {       // Works but is slow
+        int smlIndex = 0; // Buy day
+        int lrgIndex = 0; // Sell day
+        int profit = 0;
+        int counter = 1;
 
-    for(int i = 0; i < prices.Length; i++) {
-        for(int j = 1; j < prices.Length; j++) {
+        for(int i = 0; i < prices.Length; i++) {
+            for(int j = counter; j < prices.Length; j++) {
 
-            if(prices[j] - prices[i] > profit) {
-                profit = prices[j] - prices[i];
-                smlIndex = j;
-                lrgIndex = i;
+                if(prices[j] - prices[i] > profit) {
+                    profit = prices[j] - prices[i];
+                    smlIndex = j;
+                    lrgIndex = i;
+                }
             }
+            counter++;
+            
         }
-        
+        return profit;
     }
-    return $"{profit} = {smlIndex} - {lrgIndex}";
-}
-
-
-int[] prices = [7,1,5,3,6,4];
-
-Console.WriteLine(MaxProfit(prices));
