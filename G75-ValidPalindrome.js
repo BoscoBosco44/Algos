bacb3fd49f94520ab08c27fdb65a48bed13471fc
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-07-24 14:07:43 -0500

    Update G75-ValidPalindrome.js

---
+++ G75-ValidPalindrome.js
@@ -0,0 +1,11 @@
+
+//this is actualy in C#
+
+
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string input = s.replace([^0-9a-z], '');
+        return input;
+        
+    }
+}
