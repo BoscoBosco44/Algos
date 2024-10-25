986d14d345c54531e9262357edaba10977bbd5c4
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-10-25 17:59:56 -0500

    complete

--- FindClosestNumberToZero.cs
+++ FindClosestNumberToZero.cs
@@ -1,9 +1,11 @@
 int nums = [-4, -2, 1, 4, 8];
+//[-100000,-100000]
+//[2,-1,1] should return 1
 
 
 public int FindClosestNumber(int[] nums)
 {
-    int closest = nums[0];
+    int closest = Math.Abs(nums[0]);
     foreach (int num in nums)
     {
         if (Math.Abs(num) < closest)
@@ -11,7 +13,16 @@ public int FindClosestNumber(int[] nums)
             closest = Math.Abs(num);
         }
     }
-    return closest;
+
+    if (nums.Contains(closest))
+    {
+        return closest;
+    }
+    else
+    {
+        return closest * (-1);
+    }
+
 }
 
 
