3ec67d7579248869bc476a0fd92f45ec32885d75
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-10-25 17:42:36 -0500

    updates

--- FindClosestNumberToZero.cs
+++ FindClosestNumberToZero.cs
@@ -3,5 +3,17 @@ int nums = [-4, -2, 1, 4, 8];
 
 public int FindClosestNumber(int[] nums)
 {
-
+    int closest = nums[0];
+    foreach (int num in nums)
+    {
+        if (Math.Abs(num) < closest)
+        {
+            closest = Math.Abs(num);
+        }
+    }
+    return closest;
 }
+
+
+Console.WriteLine(FindClosestNumber(nums));
+
